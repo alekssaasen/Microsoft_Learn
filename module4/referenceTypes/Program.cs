0e76d386a046a86ce14ab8efe0859627b40f49e4
Aleksander Aasen <alekssaasen@gmail.com>
2026-03-22 22:50:56 +0100

    Added new project

--- module4/referenceTypes/Program.cs
+++ module4/referenceTypes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] data = new int [3];
+        
+        // when Val_b gets changed Val_A remains unaffected 
+        int val_A = 2;
+        int val_B = val_A;
+        val_B = 5;
+
+        Console.WriteLine("--Value Types--");
+        Console.WriteLine($"val_A: {val_A}");
+        Console.WriteLine($"val_B: {val_B}");
+        
+        
+        // Both points to same memory location, therefore both values changes.
+        int[] ref_A= new int[1];
+        ref_A[0] = 2;
+        int[] ref_B = ref_A;
+        ref_B[0] = 5;
+
+        Console.WriteLine("--Reference Types--");
+        Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+        Console.WriteLine($"ref_B[0]: {ref_B[0]}");
+        
     }
 }
